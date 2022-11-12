4f8a5de8bb94b09af0fc50f815a40b3a7178d838
Beth Acker <bethwherryacker@gmail.com>
2022-11-12 12:59:00 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+
+$('#reserveButton').modal({
+    keyboard: false
+  })
+
+  $('#loginButton').modal({
+    keyboard: false
+  })
